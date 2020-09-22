aaf32c95af858852b1bf1a3a46005b2ce92bcc5e
frankoadeleye <oluwatimiadeleye22@gmail.com>
2020-09-22 16:20:10 -0700

    added mobile nav

--- src/components/sidebar-menu/sidebar-menu.component.jsx
+++ src/components/sidebar-menu/sidebar-menu.component.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-// import { UserHeader } from "./sidebar-menu-header";
 import { ItemData } from "./item-data";
 import { SidebarItem } from "./item";
 import { SidebarContainer, ItemsContainer } from "./sidebar-menu.styles";
-export const HamburgerSideBar = ({ onSiginClick, onClick, ...otherStyles }) => (
+export const HamburgerSideBar = ({ onClick, ...otherStyles }) => (
   <SidebarContainer style={{ ...otherStyles }}>
-    {/* <UserHeader onSiginClick={onSiginClick} /> */}
     <ItemsContainer>
       {ItemData.map((item, index) => {
         return <SidebarItem onClick={onClick} key={index} itemData={item} />;
