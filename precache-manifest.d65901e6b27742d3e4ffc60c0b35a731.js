d78f209fad84d84b163cf16dd6ee358c7ea8e918
frankoadeleye <oluwatimiadeleye22@gmail.com>
2020-09-22 15:08:31 -0700

    Updates

---
+++ precache-manifest.d65901e6b27742d3e4ffc60c0b35a731.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22eaa3f6eaa99df238d37c872b49d2fc",
+    "url": "/wedmeplz-test/index.html"
+  },
+  {
+    "revision": "495e0ef2db4479efed3e",
+    "url": "/wedmeplz-test/static/css/main.fc92dd70.chunk.css"
+  },
+  {
+    "revision": "a3abd2bf6d505003bc2a",
+    "url": "/wedmeplz-test/static/js/2.8be6a9dd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/wedmeplz-test/static/js/2.8be6a9dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495e0ef2db4479efed3e",
+    "url": "/wedmeplz-test/static/js/main.28e56dda.chunk.js"
+  },
+  {
+    "revision": "755566498e41eb6e7529",
+    "url": "/wedmeplz-test/static/js/runtime-main.706fa325.js"
+  },
+  {
+    "revision": "7b1e176516fe49d539b4825a985c2519",
+    "url": "/wedmeplz-test/static/media/WMG-logo.7b1e1765.svg"
+  },
+  {
+    "revision": "e93e8bd8714087daa215352f9f4986a1",
+    "url": "/wedmeplz-test/static/media/banner-img.e93e8bd8.webp"
+  },
+  {
+    "revision": "320a9d7b1b215a7d16cf066fa0d459cb",
+    "url": "/wedmeplz-test/static/media/search-icon.320a9d7b.webp"
+  }
+]);
